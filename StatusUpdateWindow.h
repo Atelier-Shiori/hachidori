bb5888e123dfba8bf484d409295ca3cb9facd1da
桐間紗路 <chikorita157@chikorita157.com>
2017-06-12 08:34:50 -0700

    2.0b10 - Separate status update window to a separate window controller.

--- StatusUpdateWindow.h
+++ StatusUpdateWindow.h
@@ -7,7 +7,18 @@
 //
 
 #import <Cocoa/Cocoa.h>
-
+@class Hachidori;
 @interface StatusUpdateWindow : NSWindowController
-
+@property (strong) IBOutlet NSTextField *showtitle;
+@property (strong) IBOutlet NSPopUpButton *showstatus;
+@property (strong) IBOutlet NSPopUpButton *showscore;
+@property (strong) IBOutlet NSTextField *episodefield;
+@property (strong) IBOutlet NSNumberFormatter *epiformatter;
+@property (strong) IBOutlet NSTextView * notes;
+@property (strong) IBOutlet NSButton * isPrivate;
+@property (strong) IBOutlet NSMenu *simpleratingmenu;
+@property (strong) IBOutlet NSMenu *standardratingmenu;
+@property (strong) IBOutlet NSMenu *advancedratingmenu;
+@property (nonatomic, copy) void (^completion)(int returncode);
+- (void)showUpdateDialog:(NSWindow *) w withHachidori:(Hachidori *)haengine;
 @end
--- StatusUpdateWindow.m
+++ StatusUpdateWindow.m
@@ -7,6 +7,8 @@
 //
 
 #import "StatusUpdateWindow.h"
+#import "Hachidori.h"
+#import "AppDelegate.h"
 
 @interface StatusUpdateWindow ()
 
@@ -14,10 +16,69 @@
 
 @implementation StatusUpdateWindow
 
+- (instancetype)init{
+    self = [super initWithWindowNibName:@"StatusUpdateWindow"];
+    if(!self)
+        return nil;
+    return self;
+}
+
 - (void)windowDidLoad {
     [super windowDidLoad];
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
+- (void)showUpdateDialog:(NSWindow *) w withHachidori:(Hachidori *)haengine{
+    // Show Sheet
+    [NSApp beginSheet:self.window
+       modalForWindow:w modalDelegate:self
+       didEndSelector:@selector(updateDidEnd:returnCode:contextInfo:)
+          contextInfo:(void *)nil];
+    // Set up UI
+    _showtitle.objectValue = [haengine getLastScrobbledActualTitle];
+    // Set rating menu based on user's rating preferences
+    switch (haengine.ratingtype){
+        case ratingSimple:
+            _showscore.menu = _simpleratingmenu;
+            break;
+        case ratingStandard:
+            _showscore.menu = _standardratingmenu;
+            break;
+        case ratingAdvanced:
+            _showscore.menu = _advancedratingmenu;
+            break;
+        default:
+            _showscore.menu = _simpleratingmenu;
+            break;
+    }
+    [_showscore selectItemWithTag:[haengine getTitleScore]];
+    _episodefield.stringValue = [NSString stringWithFormat:@"%i", [haengine getCurrentEpisode]];
+    if ([haengine getTotalEpisodes]  !=0) {
+        _epiformatter.maximum = @([haengine getTotalEpisodes]);
+    }
+    [_showstatus selectItemAtIndex:[haengine getWatchStatus]];
+    _notes.string = [haengine getNotes];
+    _isPrivate.state = [haengine getPrivate];
+    // Stop Timer temporarily if scrobbling is turned on
+    AppDelegate *appdel = (AppDelegate *)[NSApplication sharedApplication].delegate;
+    if ([appdel getisScrobbling]) {
+        [appdel stoptimer];
+    }
+    
+}
+
+- (IBAction)closeupdatestatus:(id)sender {
+    [self.window orderOut:self];
+    [NSApp endSheet:self.window returnCode:0];
+}
+
+- (IBAction)updatetitlestatus:(id)sender {
+    [self.window orderOut:self];
+    [NSApp endSheet:self.window returnCode:1];
+}
+
+- (void)updateDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo {
+    self.completion(returnCode);
+}
 @end
