88c17c47c2193d9a9cacf7487193b97cba96f2e7
香風智乃 <koufuuchino@koufuuchinos-MacBook-puro.local>
2019-04-13 21:09:22 -0400

    3.1.5 - Update discordrpc, which is causing issues due to it being out of date

---
+++ DiscordRPC.framework/Versions/A/Headers/DiscordRPC.h
@@ -0,0 +1,19 @@
+//
+//  DiscordRPC.h
+//  DiscordRPC
+//
+//  Created by 小鳥遊六花 on 1/30/18.
+//  Copyright © 2018 Moy IT Solutions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DiscordRPC.
+FOUNDATION_EXPORT double DiscordRPCVersionNumber;
+
+//! Project version string for DiscordRPC.
+FOUNDATION_EXPORT const unsigned char DiscordRPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiscordRPC/PublicHeader.h>
+#import <DiscordRPC/discord_rpc.h>
+
